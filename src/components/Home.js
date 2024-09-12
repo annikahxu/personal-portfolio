148029fe69c1d1b4a4de3870564050ab0b8c7414
Annika <annika.xu19@gmail.com>
2024-09-12 11:46:01 -0400

    update resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,9 @@ export const Home = () => {
             style={{ display: "flex", marginBottom: "70px", marginTop: "20px" }}
           >
             <a href="/Resume.pdf" target="_blank" rel="noreferrer">
-              <div className="cv-button">CV</div>
+              <div className="cv-button">
+                <p>CV</p>
+              </div>
             </a>
           </div>
         </div>{" "}
